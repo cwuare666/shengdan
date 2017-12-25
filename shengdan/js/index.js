4277dbcad769f58dea687c0dcbc4a9fa8053a683
陈伟 <admin@chenweideMacBook-Air.local>
2017-12-25 09:40:37 +0800

    new

--- shengdan/js/index.js
+++ shengdan/js/index.js
@@ -23,3 +23,27 @@ $('#snow').let_it_snow({
 	image: './image/xuehua.png'
 })
 
+$('.center-people').one('animationend', function(){
+	$('.star-first').addClass('fs-one');
+	$('.peoplehide').removeClass('peoplehide');
+	$('.tophide').removeClass('tophide');
+	$('.star-second').addClass('ss-one');
+
+	$('.fs-one').one('animationend', function(){
+		$(this).addClass('fs-two');
+
+		$('.fs-two').one('animationend', function(){
+			$(this).removeClass('fs-two').addClass('fs-two-shadow fs-two-yan');
+		})
+		
+	})
+
+	setTimeout(function(){
+		$('.pdao').removeClass('pdao');
+		$('.fstran').removeClass('fstran');
+		setTimeout(function(){
+			$('.ptran').removeClass('ptran');
+		}, 1000);
+	}, 1000)
+})
+
